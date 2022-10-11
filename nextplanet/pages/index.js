44b77f3cb8d6f4116680945458cbec3a130b2a8e
Bishesh Kafle <bisheskafle27@gmail.com>
2022-10-11 20:42:49 +0545

    Added new component

--- nextplanet/pages/index.js
+++ nextplanet/pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
+import Features from "../components/Features";
 import Hero from "../components/Hero";
 import styles from "../styles/Home.module.css";
 
@@ -13,6 +14,7 @@ export default function Home() {
       </Head>
 
       <Hero />
+      <Features />
     </>
   );
 }
